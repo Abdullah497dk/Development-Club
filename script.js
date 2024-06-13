fecb05f25683b33a9a0f84c4db6316123edd524f
Abdullah Morsy <abdallamorsii@gmail.com>
2024-06-13 22:27:14 +0300

    hamburger menu eklendi

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-var swiper = new Swiper(".swiper", {
-    effect: "cards",
-    grabCursor: true,
-    initialSlide: 2,
-    speed: 500,
-    loop: true,
-    rotate: true,
-    mousewheel: {
-    invert: false,
-  },
+
+const hamMenu = document.querySelector(".ham-menu");
+
+const offScreenMenu = document.querySelector(".off-screen-menu");
+
+hamMenu.addEventListener("click", () => {
+  hamMenu.classList.toggle("active");
+  offScreenMenu.classList.toggle("active");
 });
